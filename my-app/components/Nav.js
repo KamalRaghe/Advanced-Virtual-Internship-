1595f3e2f7a4a3f84bb56698b38bafd444c03bfa
Kamal <kamal.raghe33@gmail.com>
2024-10-09 17:49:39 -0400

    commit

--- my-app/components/Nav.js
+++ my-app/components/Nav.js
@@ -3,7 +3,7 @@ import { GoSearch } from "react-icons/go";
 export default function NavBar(){
     return (
         <div style={{width:"100vw",height:"12vh",borderBottom:"1px solid lightgrey",paddingRight:"100px",display:"flex",justifyContent:"end",alignItems:"center"}} >
-            <input style={{backgroundColor:"lightgray",border:"none",borderRadius:"10px",widows:"100px",height:"20px"}} ></input>
+            <input placeholder="Search for books" style={{backgroundColor:"lightgray",border:"none",borderRadius:"2px",widows:"400px",height:"40px"}} ></input>
         </div>
     )
 }
