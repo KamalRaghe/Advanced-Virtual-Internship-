bd8421144da9615291c796f31e06d9721f5d9c9c
Kamal <kamal.raghe33@gmail.com>
2024-10-09 17:37:31 -0400

    commit

--- my-app/components/Nav.js
+++ my-app/components/Nav.js
@@ -2,6 +2,6 @@ import { GoSearch } from "react-icons/go";
 
 export default function NavBar(){
     return (
-        <div style={{width:"100%",height:"20vh",borderBottom:"1px solid grey",display:"flex",justifyContent:"end",alignItems:"center"}} >search</div>
+        <div style={{width:"100vw",height:"10vh",borderBottom:"1px solid lightgrey",paddingRight:"100px",display:"flex",justifyContent:"end",alignItems:"center"}} >search</div>
     )
 }
