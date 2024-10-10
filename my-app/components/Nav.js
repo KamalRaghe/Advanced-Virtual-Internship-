57e168c8856ad57a7652339b30bced48a72d712c
Kamal <kamal.raghe33@gmail.com>
2024-10-10 15:04:06 -0400

    commit

--- my-app/components/Nav.js
+++ my-app/components/Nav.js
@@ -2,10 +2,11 @@ import { GoSearch } from "react-icons/go";
 
 export default function NavBar(){
     return (
-        <div style={{width:"100vw",height:"12vh",borderBottom:"1px solid lightgrey",paddingRight:"100px",display:"flex",justifyContent:"end",alignItems:"center"}} >
-            border:"1px solid grey"
-            <input placeholder="Search for books" style={{backgroundColor:"#f1f6f4",paddingLeft:"25px",width:"280px",height:"40px"}} ></input>
-            <button style={{backgroundColor:"#f1f6f4",height:"40px",width:"40px",display:"flex",justifyContent:"center",alignItems:"center"}} ><GoSearch></GoSearch></button>
+        <div style={{position:'fixed',width:"100vw",height:"16vh",borderBottom:"1px solid lightgrey",paddingRight:"100px",display:"flex",justifyContent:"end",alignItems:"center"}} >
+            <input placeholder="Search for books" style={{backgroundColor:"#f1f6f4",border:"1px solid grey",paddingLeft:"20px",width:"280px",height:"40px"}} ></input>
+            <button style={{backgroundColor:"#f1f6f4",border:"1px solid grey",height:"40px",width:"40px",display:"flex",justifyContent:"center",alignItems:"center"}} >
+                <div style={{scale:"1.8",position:"relative",top:"2px"}} ><GoSearch></GoSearch></div>
+            </button>
         </div>
     )
 }
--- my-app/pages/for-you.js
+++ my-app/pages/for-you.js
@@ -1,16 +1,27 @@
 import axios from "axios"
 import SideBar from "@/components/sideBar"
 import NavBar from "@/components/Nav"
+import { useEffect, useState } from "react"
 export default function ForYou(){
+  const [books, setBooks] = useState([])
+
     async function fetchBooks(){
         const { data } = await axios.get("https://us-central1-summaristt.cloudfunctions.net/getBooks?status=recommended")
+        setBooks(data)
         console.log(data)
       }
+
+      useEffect(()=>{
+        fetchBooks()
+      },[])
       return(
         <div style={{display:"flex"}}>
              <SideBar></SideBar>
              <div>
                 <NavBar></NavBar>
+                {books.map(book =>{
+                  return <div key={book.id}>Hi</div>
+                })}
              </div>
         </div>
       )
