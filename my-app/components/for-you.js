ae6c02cb74f4db41c9df7790c87cb290875cbb11
Kamal <kamal.raghe33@gmail.com>
2024-10-10 16:19:31 -0400

    commit

--- my-app/components/for-you.js
+++ my-app/components/for-you.js
@@ -16,14 +16,16 @@ export default function ForYou(){
       },[])
       return(
         <div style={{display:"flex"}}>
-            <div style={{border:"1px solid black",}} >Recommended For You</div>
-                <div style={{display:"flex",position:"relative",right:"120px"}}>
-                    {books.map(book =>{
-                        return <div key={book.id} style={{scale:"0.28",width:"200px",height:"300px",}} >
+                <div style={{height:"100vh",width:"80%",position:"relative",left:"18vw",display:"flex",justifyContent:"start"}}>            
+                    <div style={{border:"1px solid black", width:"700px",height:"25px"}} >Recommended For You</div>
+                    <div style={{display:"flex",position:"relative",right:"220px",bottom:"50px",border:"1px solid black"}}>
+                        {books.map(book =>{
+                        return <div key={book.id} style={{scale:"0.28",width:"200px",height:"300px"}} >
                             <img src={book.imageLink}></img>
-                        </div>
-                    })}
-            </div>
+                          </div>
+                        })}
+                    </div>
+                </div>
         </div>
       )
 }
