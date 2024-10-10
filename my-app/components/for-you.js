c9dafee358e5fbf2efb3688203e71a6a3363773f
Kamal <kamal.raghe33@gmail.com>
2024-10-10 16:06:41 -0400

    commit

--- my-app/components/for-you.js
+++ my-app/components/for-you.js
@@ -1,9 +1,8 @@
 import axios from "axios"
 import SideBar from "@/components/sideBar"
 import NavBar from "@/components/Nav"
-import ForYou from "@/components/for-you"
 import { useEffect, useState } from "react"
-export default function ForYouPage(){
+export default function ForYou(){
   const [books, setBooks] = useState([])
 
     async function fetchBooks(){
@@ -17,11 +16,14 @@ export default function ForYouPage(){
       },[])
       return(
         <div style={{display:"flex"}}>
-             <SideBar></SideBar>
-             <div>
-                <NavBar></NavBar>
-                <ForYou></ForYou>
-              </div>
+            <div style={{border:"1px solid black",}} >Recommended For You</div>
+                <div style={{display:"flex",position:"relative",right:"120px"}}>
+                    {books.map(book =>{
+                        return <div key={book.id} style={{scale:"0.28",width:"200px",height:"300px",}} >
+                            <img src={book.imageLink}></img>
+                        </div>
+                    })}
+            </div>
         </div>
       )
 }
