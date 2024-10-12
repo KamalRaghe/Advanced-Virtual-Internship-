ff5fde66be6a18874a930804fa5ab3eef51f40bd
Kamal <kamal.raghe33@gmail.com>
2024-10-12 18:13:57 -0400

    commit

--- my-app/components/Books.js
+++ my-app/components/Books.js
@@ -2,7 +2,7 @@ import axios from "axios"
 import SideBar from "@/components/sideBar"
 import NavBar from "@/components/Nav"
 import { useEffect, useState } from "react"
-export default function Recommended({url}){
+export default function Books({url,name}){
   const [books, setBooks] = useState([])
 
     async function fetchBooks(){
@@ -17,7 +17,7 @@ export default function Recommended({url}){
       return(
         <div >
                 <div style={{width:"80%",position:"relative",left:"18vw",display:"flex",justifyContent:"start"}}>            
-                    <div style={{width:"700px",height:"25px",display:"flex",justifyContent:"end",color:"navy",fontSize:"20px",fontWeight:"bolder"}} >Recommended For You</div>
+                    <div style={{width:"700px",height:"25px",display:"flex",justifyContent:"end",color:"navy",fontSize:"20px",fontWeight:"bolder"}} >{name}</div>
                     <div style={{display:"flex",position:"relative",right:"240px",bottom:"50px"}}>
                         {books.map(book =>{
                         return <div key={book.id} style={{scale:"0.28",width:"200px",height:"300px"}} >
--- my-app/pages/for-you.js
+++ my-app/pages/for-you.js
@@ -1,26 +1,17 @@
 import axios from "axios"
 import SideBar from "@/components/sideBar"
 import NavBar from "@/components/Nav"
-import Recommended from "@/components/Books"
+import Books from "@/components/Books"
 import { useEffect, useState } from "react"
 export default function ForYouPage(){
   const [books, setBooks] = useState([])
-
-    async function fetchBooks(){
-        const { data } = await axios.get("https://us-central1-summaristt.cloudfunctions.net/getBooks?status=recommended")
-        setBooks(data)
-        console.log(data)
-      }
-
-      useEffect(()=>{
-        fetchBooks()
-      },[])
       return(
         <div style={{display:"flex"}}>
              <SideBar></SideBar>
              <div>
                 <NavBar></NavBar>
-                <Recommended url= {"https://us-central1-summaristt.cloudfunctions.net/getBooks?status=recommended"}/>
+                <Books url= {"https://us-central1-summaristt.cloudfunctions.net/getBooks?status=recommended"} name={'Recommended For You'}/>
+                <Books url= {"https://us-central1-summaristt.cloudfunctions.net/getBooks?status=recommended"} name={'Recommended For You'}/>
               </div>
         </div>
       )
