13d2e435ae7e129f870c60b4d28fd2e5ee438c5d
Kamal <kamal.raghe33@gmail.com>
2024-10-12 17:11:38 -0400

    commit

--- my-app/components/for-you.js
+++ my-app/components/for-you.js
@@ -18,7 +18,15 @@ export default function ForYou(){
         <div style={{display:"flex"}}>
                 <div style={{height:"100vh",width:"80%",position:"relative",left:"18vw",display:"flex",justifyContent:"start"}}>            
                     <div style={{width:"700px",height:"25px",display:"flex",justifyContent:"end",border:"1px solid black"}} >Recommended For You</div>
-                    <div style={{display:"flex",position:"relative",right:"220px",bottom:"50px"}}>
+                    <div style={{display:"flex",position:"relative",right:"240px",bottom:"50px"}}>
+                        {books.map(book =>{
+                        return <div key={book.id} style={{scale:"0.28",width:"200px",height:"300px"}} >
+                            <img src={book.imageLink}></img>
+                          </div>
+                        })}
+                    </div>
+                    <div style={{width:"700px",height:"25px",display:"flex",justifyContent:"end",border:"1px solid black"}} >Recommended For You</div>
+                    <div style={{display:"flex",position:"relative",right:"240px",bottom:"50px"}}>
                         {books.map(book =>{
                         return <div key={book.id} style={{scale:"0.28",width:"200px",height:"300px"}} >
                             <img src={book.imageLink}></img>
