3e8ced28795e988adf78faedf036f0906456fa78
Kamal <kamal.raghe33@gmail.com>
2024-10-15 14:21:12 -0400

    commit

--- my-app/components/Book.js
+++ my-app/components/Book.js
@@ -18,7 +18,9 @@ export default function Book(){
         <div >
                 <div style={{width:"80%",position:"relative",left:"18vw",margin:"20px",marginBottom:"100px"}}>            
                     <div style={{zIndex:"100",width:"700px",height:"25px",display:"flex",justifyContent:"start",color:"navy",fontSize:"20px",fontWeight:"bolder"}} >Selected just for you</div>
-                    
+                    <div style={{backgroundColor:"beige"}} >
+                        <div style={{width:"200px"}} >{book.subTitle}</div>
+                    </div>
                 </div>
         </div>
       )
