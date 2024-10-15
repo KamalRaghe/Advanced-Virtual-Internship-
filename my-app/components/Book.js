78c2ef90e744661a19fd5c6984c3d9ef5ad3f9a1
Kamal <kamal.raghe33@gmail.com>
2024-10-14 22:00:31 -0400

    commit

--- my-app/components/Book.js
+++ my-app/components/Book.js
@@ -16,10 +16,10 @@ export default function Book(){
       return(
         <div >
                 <div style={{width:"80%",position:"relative",left:"18vw",display:"flex",justifyContent:"start",margin:"20px"}}>            
-                    <div style={{width:"700px",height:"25px",display:"flex",justifyContent:"start",color:"navy",fontSize:"20px",fontWeight:"bolder"}} >Selected book</div>
-                    <div style={{display:"flex",position:"relative",bottom:"50px"}}>
+                    <div style={{width:"700px",height:"25px",display:"flex",justifyContent:"start",color:"navy",fontSize:"20px",fontWeight:"bolder"}} >Selected just for you</div>
+                    <div style={{position:"relative",bottom:"50px"}}>
                         {books.map(book =>{
-                        return <div key={book.id} style={{scale:"0.28",width:"200px",height:"300px"}} >
+                        return <div key={book.id} style={{scale:"0.28",width:"400px",height:"300px",backgroundColor:"beige"}} >
                               <img src={book.imageLink}></img>
                               <div className="center" style={{justifyContent:"start",width:"600px",color:"navy",fontSize:"60px",fontWeight:"bolder",padding:"10px"}}>{book.title}</div>
                               <div className="center" style={{justifyContent:"start",width:"600px",color:"grey",fontSize:"50px",padding:" 0px"}}>{book.author}</div>
