85acf26bfebc1c663fdb82d965e28dcf1f7847ee
Kamal <kamal.raghe33@gmail.com>
2024-10-15 12:08:47 -0400

    commit

--- my-app/components/Book.js
+++ my-app/components/Book.js
@@ -3,12 +3,12 @@ import SideBar from "@/components/sideBar"
 import NavBar from "@/components/Nav"
 import { useEffect, useState } from "react"
 export default function Book(){
-  const [books, setBooks] = useState([])
+  const [book, setBook] = useState([])
 
     async function fetchBooks(){
         const { data } = await axios.get('https://us-central1-summaristt.cloudfunctions.net/getBooks?status=selected')
-        setBooks(data)
-        console.log(data)
+        setBook(data[0])
+        console.log(data[0].subTitle)
       }
 
       useEffect(()=>{
@@ -18,20 +18,7 @@ export default function Book(){
         <div >
                 <div style={{width:"80%",position:"relative",left:"18vw",margin:"20px",marginBottom:"100px"}}>            
                     <div style={{zIndex:"100",width:"700px",height:"25px",display:"flex",justifyContent:"start",color:"navy",fontSize:"20px",fontWeight:"bolder"}} >Selected just for you</div>
-                    <div style={{position:"relative",bottom:"50px"}}>
-                        {books.map(book =>{
-                        return <div className="center" style={{backgroundColor:"beige",position:"relative",top:"70px",width:"30%"}} >
-                            <div key={book.id} className="center" style={{justifyContent:"space-between",height:"200px",width:"100%",border:"1px solid black"}} >
-                              <div >{book.subTitle}</div>
-                              <div style={{scale:"0.22"}} >
-                                    <img src={book.imageLink} style={{position:"relative"}} ></img>
-                                    <div style={{border:"1px solid black"}} >vkyu</div>
-                              </div>
-                              
-                            </div>
-                        </div>
-                        })}
-                    </div>
+                    
                 </div>
         </div>
       )
