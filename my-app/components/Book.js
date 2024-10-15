0feaa30dbc76111d416f3663b042332f1d21beb6
Kamal <kamal.raghe33@gmail.com>
2024-10-15 11:11:21 -0400

    commit

--- my-app/components/Book.js
+++ my-app/components/Book.js
@@ -15,15 +15,17 @@ export default function Book(){
       },[])
       return(
         <div >
-                <div style={{width:"80%",position:"relative",left:"18vw",display:"flex",justifyContent:"start",margin:"20px"}}>            
+                <div style={{width:"80%",position:"relative",left:"18vw",display:"flex",justifyContent:"start",margin:"20px",marginBottom:"150px"}}>            
                     <div style={{width:"700px",height:"25px",display:"flex",justifyContent:"start",color:"navy",fontSize:"20px",fontWeight:"bolder"}} >Selected just for you</div>
                     <div style={{position:"relative",bottom:"50px"}}>
                         {books.map(book =>{
-                        return <div key={book.id} style={{scale:"0.28",width:"400px",height:"300px",backgroundColor:"beige"}} >
-                              <img src={book.imageLink}></img>
+                        return <div className="center" style={{backgroundColor:"beige",position:"relative",right:"500px",top:"120px"}} >
+                            <div key={book.id} style={{scale:"0.28",width:"400px",height:"200px"}} >
+                              <img src={book.imageLink} style={{position:"relative",top:'-100px'}} ></img>
                               <div className="center" style={{justifyContent:"start",width:"600px",color:"navy",fontSize:"60px",fontWeight:"bolder",padding:"10px"}}>{book.title}</div>
                               <div className="center" style={{justifyContent:"start",width:"600px",color:"grey",fontSize:"50px",padding:" 0px"}}>{book.author}</div>
                             </div>
+                        </div>
                         })}
                     </div>
                 </div>
