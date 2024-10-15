1b93b8ef6cf64f69a93c317cc990ff48c082fe0b
Kamal <kamal.raghe33@gmail.com>
2024-10-15 11:21:23 -0400

    commit

--- my-app/components/Book.js
+++ my-app/components/Book.js
@@ -8,6 +8,7 @@ export default function Book(){
     async function fetchBooks(){
         const { data } = await axios.get('https://us-central1-summaristt.cloudfunctions.net/getBooks?status=selected')
         setBooks(data)
+        console.log(data)
       }
 
       useEffect(()=>{
@@ -15,15 +16,17 @@ export default function Book(){
       },[])
       return(
         <div >
-                <div style={{width:"80%",position:"relative",left:"18vw",display:"flex",justifyContent:"start",margin:"20px",marginBottom:"150px"}}>            
-                    <div style={{width:"700px",height:"25px",display:"flex",justifyContent:"start",color:"navy",fontSize:"20px",fontWeight:"bolder"}} >Selected just for you</div>
+                <div style={{width:"80%",position:"relative",left:"18vw",margin:"20px",marginBottom:"100px"}}>            
+                    <div style={{zIndex:"100",width:"700px",height:"25px",display:"flex",justifyContent:"start",color:"navy",fontSize:"20px",fontWeight:"bolder"}} >Selected just for you</div>
                     <div style={{position:"relative",bottom:"50px"}}>
                         {books.map(book =>{
-                        return <div className="center" style={{backgroundColor:"beige"}} >
+                        return <div className="center" style={{backgroundColor:"beige",position:"relative",top:"70px",width:"30%"}} >
                             <div key={book.id} className="center" style={{scale:"0.22",width:"400px",height:"200px"}} >
                               <img src={book.imageLink} style={{position:"relative"}} ></img>
-                              <div className="center" style={{justifyContent:"start",width:"600px",color:"navy",fontSize:"60px",fontWeight:"bolder",padding:"10px"}}>{book.title}</div>
-                              <div className="center" style={{justifyContent:"start",width:"600px",color:"grey",fontSize:"50px",padding:" 0px"}}>{book.author}</div>
+                              <div>
+                                <div className="center" style={{justifyContent:"start",width:"600px",color:"navy",fontSize:"60px",fontWeight:"bolder",padding:"10px"}}>{book.title}</div>
+                                <div className="center" style={{justifyContent:"start",width:"600px",color:"grey",fontSize:"50px",padding:" 0px"}}>{book.author}</div>
+                              </div>
                             </div>
                         </div>
                         })}
