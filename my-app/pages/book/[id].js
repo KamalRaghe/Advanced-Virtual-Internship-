19e5c738f695eff1d742b664ef3e0c96426551cc
Kamal <kamal.raghe33@gmail.com>
2024-10-20 17:23:07 -0400

    commit

--- my-app/pages/book/[id].js
+++ my-app/pages/book/[id].js
@@ -1,20 +1,11 @@
 
-import SideBar from "@/components/sideBar"
-import NavBar from "@/components/Nav"
-import Book from "@/components/selected"
-import Books from "@/components/Books"
+import BookPage from "@/components/book"
 import { useEffect, useState } from "react"
 export default function ForYouPage(){
   const [books, setBooks] = useState([])
       return(
-        <div style={{display:"flex"}}>
-             <SideBar></SideBar>
-             <div>
-                <NavBar></NavBar>
-                <div class="nav__img--mask" style={{position:"relative",left:"200px"}}>
-                    <img class="nav__img" style={{scale:"0.9"}} src="logo.png" alt="logo" />
-                </div>
-              </div>
+        <div>
+            <BookPage></BookPage>
         </div>
       )
 }
