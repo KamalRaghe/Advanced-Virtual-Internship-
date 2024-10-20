10ed04e789dbdfd7bb6da92edcaf33af31c3000b
Kamal <kamal.raghe33@gmail.com>
2024-10-20 16:55:20 -0400

    commit

--- my-app/pages/book/[id].js
+++ my-app/pages/book/[id].js
@@ -4,6 +4,12 @@ export default function Book(){
 const router = useRouter()
 const {id} = router.query 
 
+
+async function fetchBooks(){
+    const { data } = await axios.get(`https://us-central1-summaristt.cloudfunctions.net/getBook?id=${id}`)
+    setBook(data[0])
+  }
+
 return(
     <div>
         {id}
