e5f8df73b55d5844846825b7a595b8f3d33e78e1
Kamal <kamal.raghe33@gmail.com>
2024-10-20 17:09:31 -0400

    commit

---
+++ my-app/pages/book/[id].js
@@ -0,0 +1,17 @@
+
+import SideBar from "@/components/sideBar"
+import NavBar from "@/components/Nav"
+import Book from "@/components/selected"
+import Books from "@/components/Books"
+import { useEffect, useState } from "react"
+export default function ForYouPage(){
+  const [books, setBooks] = useState([])
+      return(
+        <div style={{display:"flex"}}>
+             <SideBar></SideBar>
+             <div>
+                <NavBar></NavBar>
+              </div>
+        </div>
+      )
+}
