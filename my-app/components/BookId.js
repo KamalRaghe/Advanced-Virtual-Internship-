5453b609ebcd5b5caee6cd0cb493d29da66d2846
Kamal <kamal.raghe33@gmail.com>
2024-10-23 13:47:50 -0400

    commit

--- my-app/components/BookId.js
+++ my-app/components/BookId.js
@@ -7,27 +7,29 @@ import { CiStar } from "react-icons/ci";
 import { useRouter } from "next/router";
 
 export default function BookId({url,name,move,subName}){
-  const [book, setbook] = useState([])
+  const [book, setBook] = useState([])
   const router = useRouter()
 
-    async function fetchbook(){
+    async function fetchBook(){
         const { data } = await axios.get(url)
-        setbook(data)
+        setBook(data)
         console.log(data)
       }
 
       useEffect(()=>{
-        fetchbook()
+        fetchBook()
       },[])
       return(
         <div >
-                <div className="bookcreen" style={{border:"1px solid black",position:"relative",display:"flex",justifyContent:"start"}}>            
+                <div className="BookScreen" style={{position:"relative",display:"flex",justifyContent:"space-between"}}>            
                     <div>
                         <div style={{width:"700px",display:"flex",justifyContent:"start",color:"navy",fontSize:"30px",margin:"10px",fontWeight:"bolder"}} >{book.title}</div>
                         <div style={{marginLeft:"10px"}} >{book.author}</div>
                         <div style={{margin:"10px",fontSize:"20px"}} >{book.subTitle}</div>
                     </div>
-                    <img style={{scale:"0.5"}} src={book.imageLink}></img>
+                    <div style={{border:"1px solid black"}} >
+                        <img style={{border:"1px solid black",scale:"0.29",width:"200px",height:"300px"}} src={book.imageLink}></img>
+                    </div>
                       
         
                 </div>
