793c4cacf0e9571d0fa0334102de85fd1feb56ca
Kamal <kamal.raghe33@gmail.com>
2024-10-23 14:03:02 -0400

    commit

--- my-app/components/BookId.js
+++ my-app/components/BookId.js
@@ -24,11 +24,12 @@ export default function BookId({url,name,move,subName}){
                 <div className="BookScreen" style={{position:"relative",display:"flex",justifyContent:"space-between"}}>            
                     <div>
                         <div style={{width:"700px",display:"flex",justifyContent:"start",color:"navy",fontSize:"30px",margin:"10px",fontWeight:"bolder"}} >{book.title}</div>
-                        <div style={{marginLeft:"10px"}} >{book.author}</div>
+                        <div style={{margin:"10px",fontWeight:"bold"}} >{book.author}</div>
                         <div style={{margin:"10px",fontSize:"20px"}} >{book.subTitle}</div>
+                        <div className="center" style={{width:"150px",justifyContent:"space-between"}}  ><CiStar style={{scale:"1.5"}} /> {book.averageRating} ({book.totalRating} ratings)</div>
                     </div>
-                    <div style={{border:"1px solid black"}} >
-                        <img style={{border:"1px solid black",scale:"0.29",width:"200px",height:"300px"}} src={book.imageLink}></img>
+                    <div>
+                        <img style={{width:"300px",height:"300px"}} src={book.imageLink}></img>
                     </div>
                       
         
