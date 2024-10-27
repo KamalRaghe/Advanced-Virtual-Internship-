5ce19446508e06300d7e028f028e8a43c4eb0668
Kamal <kamal.raghe33@gmail.com>
2024-10-27 17:01:16 -0400

    commit

--- my-app/components/BookId.js
+++ my-app/components/BookId.js
@@ -57,9 +57,11 @@ export default function BookId({url,name,move,subName}){
                         </div>
                         <div className="side-bar__icon" style={{fontSize:"20px",color:"blue",padding:"20px 0px"}} ><CiBookmark></CiBookmark> <div style={{margin:"5px"}} ></div>Add title to My library</div>
                         <div style={{fontSize:"18px",fontWeight:"bold",paddingBottom:"20px"}}>What's it about?</div>
-                        {tags.map(tag =>{
-                            return <div key={tag} style={{margin:"10px",display:"flex",color:"red"}}><button>{tag}</button></div>
-                        })}
+                        <div style={{display:"flex"}}>
+                            {tags.map(tag =>{
+                                return <div key={tag} style={{fontSize:"18px",margin:"10px 0px",marginRight:"10px",display:"flex",backgroundColor:"#f1f6f4",padding:'20px'}}><button>{tag}</button></div>
+                            })}
+                        </div>
                     </div>
                     <div>
                         <img style={{width:"300px",height:"300px",margin:"10px"}} src={book.imageLink}></img>
