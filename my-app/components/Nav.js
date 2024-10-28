799dfcbbb82c6d247cb57e25aec3282f3b9cab33
Kamal <kamal.raghe33@gmail.com>
2024-10-28 11:20:27 -0400

    commit

--- my-app/components/Nav.js
+++ my-app/components/Nav.js
@@ -21,7 +21,7 @@ export default function NavBar(){
         <div style={{width:"100vw",height:"16vh",borderBottom:"1px solid lightgrey",paddingRight:"100px",display:"flex",justifyContent:"end",alignItems:"center"}} >
             <input placeholder="Search for books" value={search} onChange = {(e) => {setSearch(e.target.value);console.log(e.target.value)}}
             style={{backgroundColor:"#f1f6f4",border:"1px solid grey",paddingLeft:"20px",width:"280px",height:"40px"}} ></input>
-            <button onClick={()=>console.log('e')} style={{backgroundColor:"#f1f6f4",border:"1px solid grey",height:"40px",width:"40px",display:"flex",justifyContent:"center",alignItems:"center"}} >
+            <button onClick={()=>console.log('e')} style={{zIndex:'100',backgroundColor:"#f1f6f4",border:"1px solid grey",height:"40px",width:"40px",display:"flex",justifyContent:"center",alignItems:"center"}} >
                 <div style={{scale:"1.8",position:"relative",top:"2px"}} ><GoSearch></GoSearch></div>
             </button>
         </div>
