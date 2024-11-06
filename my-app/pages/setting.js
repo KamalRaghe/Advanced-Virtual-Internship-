9dc21c8e9ab64a85fdc739b254a1ff4d89a5a788
Kamal <kamal.raghe33@gmail.com>
2024-11-06 15:38:58 -0500

    commit

---
+++ my-app/pages/setting.js
@@ -0,0 +1,10 @@
+export default function Setting(){
+        return(
+          <div style={{display:"flex"}}>
+               <SideBar></SideBar>
+               <div className="BookScreen">
+                  
+                </div>
+          </div>
+        )
+  }
