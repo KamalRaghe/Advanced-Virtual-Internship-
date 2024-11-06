1e2eea06b4925ffaa67fc88aba3a463f9d351aab
Kamal <kamal.raghe33@gmail.com>
2024-11-06 15:39:54 -0500

    commit

--- my-app/pages/setting.js
+++ my-app/pages/setting.js
@@ -1,9 +1,11 @@
+import SideBar from "@/components/sideBar"
+
 export default function Setting(){
         return(
           <div style={{display:"flex"}}>
                <SideBar></SideBar>
                <div className="BookScreen">
-                  
+                    Settings
                 </div>
           </div>
         )
