f4d9d2127ce133ef97a419343e5c14daf87a6e21
Kamal <kamal.raghe33@gmail.com>
2024-11-07 14:33:15 -0500

    commit

--- my-app/pages/setting.js
+++ my-app/pages/setting.js
@@ -5,13 +5,17 @@ export default function Setting(){
         return(
           <div style={{display:"flex"}}>
                <SideBar></SideBar>
-               <div className="BookScreen">
+               <div >
                     <NavBar></NavBar>
-                    <div style={{color:"#032b41",fontSize:"32px",fontWeight:"bolder",margin:"10px",marginBottom:"40px",padding:"20px 0px",borderBottom:"1px solid lightgrey"}} >Settings</div>
-                    <div style={{color:"#032b41",fontSize:"24px",fontWeight:"bolder"}}>Your Subscription plan</div>
-                    <div>premium-plus</div>
-                    <div>Email</div>
-                    <div>hanna@gmail.com</div>
+                    <div className="BookScreen">
+                        <div style={{color:"#032b41",fontSize:"32px",fontWeight:"bolder",margin:"10px",marginBottom:"30px",padding:"20px 0px",borderBottom:"1px solid lightgrey"}} >Settings</div>
+                        <div style={{marginLeft:"10px"}}>
+                            <div style={{color:"#032b41",fontSize:"18px",fontWeight:"bolder"}}>Your Subscription plan</div>
+                            <div style={{color:"#032b41",paddingTop:"10px",paddingBottom:"20px",borderBottom:"1px solid lightgrey"}} >premium-plus</div>
+                            <div style={{color:"#032b41",fontSize:"18px",fontWeight:"bolder",marginTop:"30px",paddingBottom:"10px"}} >Email</div>
+                            <div>hanna@gmail.com</div>
+                        </div>
+                    </div>
                 </div>
           </div>
         )
