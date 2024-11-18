fa00b85d4ded731ba061a1a4de7a0a0e73224d94
Kamal <kamal.raghe33@gmail.com>
2024-11-18 15:05:00 -0500

    commit

--- my-app/components/selected.js
+++ my-app/components/selected.js
@@ -6,10 +6,12 @@ import { FaPlay } from "react-icons/fa";
 import Time from "./time";
 export default function Book(){
   const [book, setBook] = useState([])
+  const [move, setMove] = useState('23px')
 
     async function fetchBooks(){
         const { data } = await axios.get('https://us-central1-summaristt.cloudfunctions.net/getBooks?status=selected')
         setBook(data[0])
+        // setMove('0px')
       }
 
       useEffect(()=>{
@@ -24,15 +26,15 @@ export default function Book(){
                         <div className="center" style={{borderRight:"1px solid lightblue",height:"90%",width:"10px"}} ></div>
                         {!book ? <div key={book.id} style={{scale:"0.24",width:"200px",height:"300px",position:"relative",bottom:"30px"}} >
                             <img src={book.imageLink}></img>
-                        </div>:<div className="skeleton" style={{width:"150px",height:"150px",margin:"20px"}}></div>}
+                        </div>:<div className="skeleton" style={{width:"150px",height:"150px",margin:"20px",position:"relative",left:"40px"}}></div>}
                         <div style={{height:"100%",width:"300px",position:"relative",left:"20px",top:"10px"}} >
-                            {!book ? <div className="center" style={{justifyContent:"start",fontWeight:"bolder"}}>{book.title}</div>:<div className="skeleton" style={{width:"75px", height:"20px"}} ></div>}
-                            {!book ? <div className="center" style={{justifyContent:"start",scale:"0.8",position:"relative",right:"20px"}}>{book.author}</div>:<div className="skeleton" style={{width:"45px", height:"12px",marginTop:"10px"}} ></div>}
+                            {!book ? <div className="center" style={{justifyContent:"start",fontWeight:"bolder"}}>{book.title}</div>:<div className="skeleton" style={{width:"75px", height:"20px",position:"relative",left:"30px"}} ></div>}
+                            {!book ? <div className="center" style={{justifyContent:"start",scale:"0.8",position:"relative",right:"20px"}}>{book.author}</div>:<div className="skeleton" style={{width:"45px", height:"15px",marginTop:"10px",position:"relative",left:"30px"}} ></div>}
                             <div style={{display:"flex",alignItems:"center",marginTop:"20px"}} >
-                              <div className="center" style={{position:"relative",backgroundColor:"black",color:"white",borderRadius:"50%",width:'40px',height:'40px'}}>
+                              <div className="center" style={{position:"relative",left: move,backgroundColor:"black",color:"white",borderRadius:"50%",width:'40px',height:'40px'}}>
                                 <FaPlay></FaPlay> 
                               </div>
-                              {!book ? <div style={{padding:'10px'}} >3 mins 23 secs</div>:<div style={{padding:'10px'}} >0 mins 00 secs</div>}
+                              {!book ? <div style={{padding:'10px'}} >3 mins 23 secs</div>:<div style={{padding:'10px',position:"relative",left:"20px"}} >0 mins 00 secs</div>}
                             </div>
                         </div>
                     </div>
