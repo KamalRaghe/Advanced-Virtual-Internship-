02f22f06575525e46f10bf584d9f0f96b1930917
Kamal <kamal.raghe33@gmail.com>
2024-11-19 14:54:11 -0500

    commit

--- my-app/pages/player/[id].js
+++ my-app/pages/player/[id].js
@@ -8,7 +8,7 @@ import Player from "@/components/player"
 import AudioPlayer from "@/components/audio"
 
 export default function ForYouPage(){
-  const [book,setBook] = useState()
+  const [loaded,setLoaded] = useState()
   const [title, setTitle] = useState()
   const [author, setAuthor] = useState()
   const [audioLink, setAudioLink] = useState()
@@ -16,14 +16,12 @@ export default function ForYouPage(){
   const router = useRouter()
   const {id} = router.query
 
-  async function fetchBook(){
-    const { data } = await axios.get(`https://us-central1-summaristt.cloudfunctions.net/getBook?id=${id}`)
-    setBook(data)
-    console.log(audioLink)
-  }
+ 
 
   useEffect(()=>{
-    fetchBook()
+    setTimeout(() => {
+      setLoaded(true)
+    }, 1000);
       setTitle(window.localStorage.getItem('title'))
       setAuthor(window.localStorage.getItem('author'))
       setAudioLink(window.localStorage.getItem('audioLink'))
@@ -35,7 +33,7 @@ export default function ForYouPage(){
       return(
         <div style={{display:"flex"}}>
              <SideBar small={true} ></SideBar>
-             <AudioPlayer audioUrl={audioLink} title = {title} img = {image} author={author}/>
+             {loaded ? <AudioPlayer audioUrl={audioLink} title = {title} img = {image} author={author}/>:<div className="skeleton" style={{width:"100%",height:"100px",position:"fixed",top:"87%"}}></div>}
              <div>
                 <NavBar></NavBar>
                 <Player url={id}/>
