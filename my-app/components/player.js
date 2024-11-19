15bbaff7348996a08cb363cac1e14806651503a3
Kamal <kamal.raghe33@gmail.com>
2024-11-19 14:47:45 -0500

    commit

--- my-app/components/player.js
+++ my-app/components/player.js
@@ -30,15 +30,12 @@ export default function Player(){
         <div >
                 <div className="BookScreen" style={{display:"flex",justifyContent:"space-between"}}>            
                     <div style={{padding:"10px"}} >
-                        <div style={{width:"600px",display:"flex",justifyContent:"start",color:"#032b41",fontSize:"40px",margin:"10px",fontWeight:"bolder"}}>{title}</div>
-                        <div style={{margin:"20px 10px",fontWeight:"bold"}} >{author}</div>
-                        <div style={{margin:"10px",fontSize:"25px"}} >{subTitle}</div>
+                        <div style={{width:"600px",display:"flex",justifyContent:"start",color:"#032b41",fontSize:"40px",margin:"10px",fontWeight:"bolder"}}>{title ? title : <div className="skeleton" style={{width:"600px",height:"100px"}}></div>}</div>
+                        <div style={{margin:"20px 10px",fontWeight:"bold"}} >{author ? author : <div className="skeleton" style={{width:"150px",height:"30px"}}></div>}</div>
+                        <div style={{margin:"10px",fontSize:"25px"}} >{subTitle ? subTitle : <div className="skeleton" style={{width:"600px",height:"50px"}}></div>}</div>
                         <div>
-                            {!summary ? summary : <div className="skeleton" style={{width:"800px",height:"700px"}} ></div>}
+                            {summary ? summary : <div className="skeleton" style={{width:"1000px",height:"700px"}} ></div>}
                         </div>
-                        
-
-                      
                     </div>
                     <div>
                     </div>
