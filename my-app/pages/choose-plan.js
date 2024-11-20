66fa62a55cae3aa1eabfd7ecc33787df19ed93c2
Kamal <kamal.raghe33@gmail.com>
2024-11-20 13:50:06 -0500

    commit

--- my-app/pages/choose-plan.js
+++ my-app/pages/choose-plan.js
@@ -6,9 +6,12 @@ import Books from "@/components/Books"
 import { useEffect, useState } from "react"
 export default function ForYouPage(){
   const [books, setBooks] = useState([])
+  console.log('dvwvwre')
       return(
         <div>
-             
+             <div className="center"  style={{alignItems:"end",height:"90vh",width:"100vw",backgroundColor:"#032b41",borderBottomRightRadius:"40%",borderBottomLeftRadius:"40%"}}>
+                <div style={{height:"300px",width:"300px",backgroundColor:"white",borderTopRightRadius:"50%",borderTopLeftRadius:"50%"}} ></div>
+            </div>
         </div>
       )
 }
