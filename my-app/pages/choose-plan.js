90a27ed77376fc46509ea42976b521cfa2672005
Kamal <kamal.raghe33@gmail.com>
2024-11-20 14:04:42 -0500

    commit

--- my-app/pages/choose-plan.js
+++ my-app/pages/choose-plan.js
@@ -9,8 +9,10 @@ export default function ForYouPage(){
   console.log('dvwvwre')
       return(
         <div>
-             <div className="center"  style={{alignItems:"end",height:"90vh",width:"100vw",backgroundColor:"#032b41",borderBottomRightRadius:"40%",borderBottomLeftRadius:"40%"}}>
-                <div style={{height:"300px",width:"300px",backgroundColor:"white",borderTopRightRadius:"50%",borderTopLeftRadius:"50%"}} ></div>
+             <div className="center" style={{zIndex:"100",alignItems:"end",height:"95vh",width:"100vw",backgroundColor:"#032b41",borderBottomRightRadius:"30%",borderBottomLeftRadius:"30%"}}>
+                <div className="center" style={{zIndex:"2",height:"300px",width:"350px",backgroundColor:"white",borderTopRightRadius:"50%",borderTopLeftRadius:"50%"}} >
+                    <div style={{scale:"0.4",zIndex:"1"}} ><img src="pricing-top.png" alt="logo" /></div>
+                </div>
             </div>
         </div>
       )
