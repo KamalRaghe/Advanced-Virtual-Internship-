d46984e72f776274a55c87a9088235865bdee396
Kamal <kamal.raghe33@gmail.com>
2024-11-20 14:16:22 -0500

    commit

--- my-app/pages/choose-plan.js
+++ my-app/pages/choose-plan.js
@@ -9,9 +9,11 @@ export default function ForYouPage(){
   console.log('dvwvwre')
       return(
         <div>
-             <div className="center" style={{zIndex:"100",alignItems:"end",height:"95vh",width:"100vw",backgroundColor:"#032b41",borderBottomRightRadius:"30%",borderBottomLeftRadius:"30%"}}>
+             <div className="center" style={{zIndex:"100",flexDirection:"column",justifyContent:"space-between",height:"95vh",width:"100vw",backgroundColor:"#032b41",borderBottomRightRadius:"30%",borderBottomLeftRadius:"30%"}}>
+                <div style={{fontSize:"50px",width:"1000px",color:"white",marginTop:"40px",fontWeight:"bolder"}} >Get unlimited access to many amazing books to read</div>
+                <div>Turn ordinary moments into amazing learning opportunities</div>
                 <div className="center" style={{zIndex:"2",height:"300px",width:"350px",backgroundColor:"white",borderTopRightRadius:"50%",borderTopLeftRadius:"50%"}} >
-                    <div style={{scale:"0.4",zIndex:"1"}} ><img src="pricing-top.png" alt="logo" /></div>
+                    <img style={{scale:"0.4",zIndex:"1",borderRadius:"50%"}} src="pricing-top.png" alt="logo" />
                 </div>
             </div>
         </div>
