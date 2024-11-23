51487fb7646e54753e1f5414c92103b8827dc1ee
Kamal <kamal.raghe33@gmail.com>
2024-11-23 17:07:17 -0500

    commit

--- my-app/pages/choose-plan.js
+++ my-app/pages/choose-plan.js
@@ -16,20 +16,7 @@ export default function ForYouPage(){
                 </div>
             </div>
             <div className="center"> 
-                <div className="center" style={{justifyContent:'space-between',width:"250px",scale:"3",paddingTop:"40px"}} >
-                    <div className="center" style={{flexDirection:"column"}} >
-                        <LuFileKey/>
-                        <span style={{fontSize:"5px",paddingTop:"10px", width:"70px",textAlign:"center"}} >Key ideas in few min with many books to read</span>
-                    </div>
-                    <div className="center" style={{flexDirection:"column"}} >
-                        <PiPottedPlantFill/>
-                        <span style={{fontSize:"5px",paddingTop:"10px", width:"70px",textAlign:"center"}} >3 million people growing with Summarist everyday</span>
-                    </div>
-                    <div className="center" style={{flexDirection:"column"}}>
-                        <FaHandshake/>
-                        <span style={{fontSize:"5px",paddingTop:"10px", width:"70px",textAlign:"center"}} >Precise recommendations collections curated by experts</span>
-                    </div>
-                </div>
+               
             </div>
         </div>
       )
